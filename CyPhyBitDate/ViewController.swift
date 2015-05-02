d3fb3d71ad08ff1f0567b682894377d8f5daf473
Matt Linaberry <mtlinaberry@gmail.com>
2015-05-02 16:13:03 -0400

    Initial Commit

---
+++ CyPhyBitDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CyPhyBitDate
+//
+//  Created by Matt Linaberry on 5/2/15.
+//  Copyright (c) 2015 Matt Linaberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
